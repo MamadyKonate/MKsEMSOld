9ea1c092bcc0c5abac86901075416719943b984b
Mamady Konate <L00177597@atu.ie>
2023-04-09 17:30:40 +0100

    Utilities class added to the project. Further constraints added to FirsName and LastName fields. IsUserLoggedIn field added to User

---
+++ Controllers/Utilities.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MKsEMS.Controllers
+{
+    public class Utilities : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
