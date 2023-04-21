5cecb3eea89d937c23f54de8327c925c32e4e539
Mamady Konate <L00177597@atu.ie>
2023-04-22 00:20:29 +0100

    syncing from remote to remove some issues

--- Controllers/UserLoginsController.cs
+++ Controllers/UserLoginsController.cs
@@ -98,6 +98,12 @@ namespace MKsEMS.Controllers
                     }
 
                 }
+                else //no user found in the system with the same email as entered
+                {
+                    _loggedInUser.GetLoggedInUser().IsUserLoggedIn = false;
+                    TempData["Message"] = "Incorrect Username or Password entered " + (_incorrectPasswordEntered += 1) + " time(s).";
+                    return View();
+                }
             }
 
             return View("Index");
