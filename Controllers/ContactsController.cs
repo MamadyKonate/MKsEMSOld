8457664a6aabe5d3814150df6aae1c21c7584194
Mamady Konate <L00177597@atu.ie>
2023-05-04 01:12:42 +0100

    Recreating Xunit project

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -59,43 +59,7 @@ namespace MKsEMS.Controllers
 
             return View(contact);
         }
-
-        ///GET: Contacts/Create
-        //public IActionResult Create()
-        //{
-        //    if(!AdminUserIsLoggedIn())
-        //    {
-        //        TempData["AdminMessage"] = "Please login as an Administrator";
-        //        return RedirectToAction("Index", "Users");
-        //    }
-
-        //    return View();
-        //}
-
-        //// POST: Contacts/Create
-        //// To protect from overposting attacks, enable the specific properties you want to bind to.
-        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create([Bind("Id,AddressLine1,AddressLine2,AddressLine3,City,County,Eircode,Phone,UserEmail")] Contact contact)
-        //{
-        //    if (!AdminUserIsLoggedIn())
-        //    {
-        //        TempData["AdminMessage"] = "Please login as an Administrator";
-        //        return RedirectToAction("Index", "Users");
-        //    }
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        _context.Add(contact);
-        //        await _context.SaveChangesAsync();
-        //        var user = _context.Users.FirstOrDefault(u => u.Email == contact.UserEmail);
-
-        //        return RedirectToAction("index" , "Users");
-        //    }
-
-        //    return View(contact);
-        //}
+        
 
         // GET: Contacts/Edit/5
         public async Task<IActionResult> Edit(int? id)
@@ -158,56 +122,7 @@ namespace MKsEMS.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(contact);
-        }
-
-        //// GET: Contacts/Delete/5
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (!AdminUserIsLoggedIn())
-        //    {
-        //        TempData["AdminMessage"] = "Please login as an Administrator";
-        //        return RedirectToAction("Index", "Users");
-        //    }
-
-        //    if (id == null || _context.Contacts == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var contact = await _context.Contacts
-        //        .FirstOrDefaultAsync(m => m.Id == id);
-        //    if (contact == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return View(contact);
-        //}
-
-        //// POST: Contacts/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> DeleteConfirmed(int id)
-        //{
-        //    if (!AdminUserIsLoggedIn())
-        //    {
-        //        TempData["AdminMessage"] = "Please login as an Administrator";
-        //        return RedirectToAction("Index", "Users");
-        //    }
-            
-        //    if (_context.Contacts == null)
-        //    {
-        //        return Problem("Entity set 'EMSDbContext.Contacts'  is null.");
-        //    }
-        //    var contact = await _context.Contacts.FindAsync(id);
-        //    if (contact != null)
-        //    {
-        //        _context.Contacts.Remove(contact);
-        //    }
-
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction(nameof(Index));
-        //}
+        }                
 
         private List<Contact> GetRelevantContacts()
         {
---
+++ UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace MKsEMS;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
